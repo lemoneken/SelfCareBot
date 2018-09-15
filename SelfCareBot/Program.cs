c287183c9ad8ff2fe862d2367d5200f2ef445137
nktitorov <nktitorov@dataart.com>
2018-09-15 03:11:08 +0300

    Created a .NET Core solution for bot

---
+++ SelfCareBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfCareBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
